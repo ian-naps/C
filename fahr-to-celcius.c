f345faf5a88f91645ed2d6ff482ab33550eca0ce
[Ian Napoli] <[ian.napoli8@gmail.com]>
2023-11-01 23:55:39 -0700

    improvements to fahr-celsius program & corresponding celsius-fahr conversion program

--- fahr-to-celcius.c
+++ fahr-to-celcius.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-/*Print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300 */
+/*Print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300 ; floating point version*/
 
 main()
 {
-    int fahr, celsius;
-    int lower, upper, step;
+    float fahr, celsius;
+    float lower, upper, step;
 
     lower = 0; /* lower limit of temperature scale*/
     upper = 300; /* upper limit*/
     step = 20; /* step size*/
 
     fahr = lower;
+    printf("Fahrenheit -> Celsius\n");
     while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%d\t%d\n" , fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%5.0f %15.2f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
